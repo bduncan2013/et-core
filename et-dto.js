50bcec6d2db64e247756a4dd7f3d5b0ff91f0744
bduncan2013 <bill.duncan@charter.net>
2014-01-03 08:24:05 -0500

    cleaned up functions

--- et-dto.js
+++ et-dto.js
@@ -2,94 +2,95 @@ exports.sysdto = sysdto = function sysdto(params, callback) {
 
     // create systemdto
     executearray([{
-        "executetis":"addwidmaster",
-        "metadata.method":"systemdto",
-        "wid":"systemdto",
-        "creator":"accounttype",
-        "created":"datetime",
-        "expiration":"datetime",
-        "category":"string",
-        "subcategory":"string",
-        "securitydto":"onetomany",
-        "syncdto":"onetomany",
-        "statusdto":"onetomany",
-        "balancedto":"onetomany"
-    },{
+        "executetis": "addwidmaster",
+        "metadata.method": "systemdto",
+        "wid": "systemdto",
+        "creator": "accounttype",
+        "created": "datetime",
+        "expiration": "datetime",
+        "category": "string",
+        "subcategory": "string",
+        "securitydto": "onetomany",
+        "syncdto": "onetomany",
+        "statusdto": "onetomany",
+        "balancedto": "onetomany"
+    }, {
 
-    // create relationships
-        "executethis":"addwidmaster",
-        "wid":"relationshipdto1",
-        "metadata.method":"relationshipdto",
-        "primarywid":"systemdto",
-        "secondarywid":"securitydto"
-    },{
-        "executethis":"addwidmaster",
-        "wid":"relationshipdto2",
-        "metadata.method":"relationshipdto",
-        "primarywid":"systemdto",
-        "secondarywid":"syncdto"
-    },{
-        "executethis":"addwidmaster",
-        "wid":"relationshipdto3",
-        "metadata.method":"relationshipdto",
-        "primarywid":"systemdto",
-        "secondarywid":"statusdto"
-    },{
-        "executethis":"addwidmaster",
-        "wid":"relationshipdto4",
-        "metadata.method":"relationshipdto",
-        "primarywid":"systemdto",
-        "secondarywid":"balancedto"
-    }]
+        // create relationships
+        "executethis": "addwidmaster",
+        "wid": "relationshipdto1",
+        "metadata.method": "relationshipdto",
+        "primarywid": "systemdto",
+        "secondarywid": "securitydto"
+    }, {
+        "executethis": "addwidmaster",
+        "wid": "relationshipdto2",
+        "metadata.method": "relationshipdto",
+        "primarywid": "systemdto",
+        "secondarywid": "syncdto"
+    }, {
+        "executethis": "addwidmaster",
+        "wid": "relationshipdto3",
+        "metadata.method": "relationshipdto",
+        "primarywid": "systemdto",
+        "secondarywid": "statusdto"
+    }, {
+        "executethis": "addwidmaster",
+        "wid": "relationshipdto4",
+        "metadata.method": "relationshipdto",
+        "primarywid": "systemdto",
+        "secondarywid": "balancedto"
+    }]);
 
 }
 
 exports.initdto = initdto = function initdto(params, callback) {
 
     // create groupdto
-    executearray("addwidmaster", {
+    executearray([{
+        "executetis": "addwidmaster",
         "metadata.method": "groupdto",
         "wid": "groupdto",
         "category": "categorytype",
         "subcategory": "subcategorytype",
         "group": "grouptype"
-    }, "", "");
+    }, {
 
-    // create securitydto
-    executearray("addwidmaster", {
+        // create securitydto
+        "executethis": "addwidmaster",
         "metadata.method": "securitydto",
         "wid": "securitydto",
         "securitytype": "actiontype",
         "group": "grouptype",
         "expiration": "datetime"
-    }, "", "");
+    }, {
 
-    // create syncdto
-    executearray("addwidmaster", {
+        // create syncdto
+        "executethis": "addwidmaster",
         "metadata.method": "syncdto",
         "wid": "syncdto",
         "expiration": "datetime",
         "offlinerule": "offlineruletype",
         "onlinerule": "onlineruletype",
         "slowrule": "slowruletype"
-    }, "", "");
+    }, {
 
-    // create statusdto
-    executearray("addwidmaster", {
+        // create statusdto
+        "executethis": "addwidmaster",
         "metadata.method": "statusdto",
         "wid": "statusdto",
         "accesstoken": "string",
         "userstatus": "integer",
         "devicestatus": "integer"
-    }, "", "");
+    }, {
 
-    // create balancedto
-    executearray("addwidmaster", {
+        // create balancedto
+        "executethis": "addwidmaster",
         "metadata.method": "balancedto",
         "wid": "balancedto",
         "widname": "wid",
         "balance": "integer"
-    }, "", "");
+    }]);
 }
 
 exports.testdto = testdto = function testdto(params, callback) {
