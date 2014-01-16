ab7e933097c577a382b0c3dbaf4692a950d004e9
JaySturm <jaysturm@gmail.com>
2014-01-16 10:01:53 -0700

    updating ngWidApp and adding et-converttoscreenwid.js which converts webpages to screenWids

---
+++ et-converttoscreenwid.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+    var fileName = window.location.href.split('/').pop(),
+        widName = fileName.substr(0, fileName.indexOf('.')),
+        params = {};
+
+    if (typeof widforview != 'undefined') { params.widforview = widforview; }
+    if (typeof widforbase != 'undefined') { params.widforbase = widforbase;}
+    if (typeof widforbackground != 'undefined') { params.widforbackground = widforbackground; }
+    if (typeof dataforview != 'undefined') { params.dataforview = dataforview; }
+    if (typeof links != 'undefined') { params.links = links; }
+
+    // append any existing style blocks to body before continuing
+    $('style').each(function() { $('body').append(this.outerHTML); });
+
+    // convert linked html page to a screenwid
+    htmlToScreenwid(widName, $('body').html(), params);
+
+//    window.location = 'http://dripoint.com?wid=' + widName;
+});
