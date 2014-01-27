9aebfa02f0f4dc31bd396d2b1f3433f312d5bed5
bduncan2013 <bill.duncan@charter.net>
2014-01-27 15:59:02 -0500

    Put things back tested did not yield needed results.

--- et-converttoscreenwid.js
+++ et-converttoscreenwid.js
@@ -4,25 +4,16 @@ $(document).ready(function () {
         params = {};
 
     if (typeof widforview != 'undefined') { params.widforview = widforview; }
-    if (typeof widforbase != 'undefined') { params.widforbase = widforbase; }
+    if (typeof widforbase != 'undefined') { params.widforbase = widforbase;}
     if (typeof widforbackground != 'undefined') { params.widforbackground = widforbackground; }
     if (typeof dataforview != 'undefined') { params.dataforview = dataforview; }
     if (typeof links != 'undefined') { params.links = links; }
 
-    // append any existing style blocks to body before continuing 
+    // append any existing style blocks to body before continuing
     $('style').each(function () { $('body').append(this.outerHTML); });
 
-    // convert linked html page to a screenwid 
+    // convert linked html page to a screenwid
     htmlToScreenwid(widName, $('body').html(), params);
 
-        var executeObj = {executethis:'getwidmaster',wid:widName}; 
-        execute(executeObj, function(err, resultArr) { 
-            var returnedHtml = ''; 
-            screenwidToHtml(resultArr.shift(), function(htmlString) { 
-                returnedHtml = htmlString; 
-                var pause = ''; 
-            }); 
-        }); 
-
-        window.location = 'http://dripoint.com?wid=' + widName; 
+    window.location = 'http://dripoint.com?wid=' + widName;
 });
