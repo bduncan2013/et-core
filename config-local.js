6586c0bd13847a7aadc8665ce8b8fa1a3fe11756
JaySturm <jaysturm@gmail.com>
2014-03-26 14:49:16 -0600

    changed offlineget wid to getFromAngular in the default configuration of config-local

--- config-local.js
+++ config-local.js
@@ -265,7 +265,8 @@ function config123() {
     configuration.getwid[0] = {};
     configuration.getwid[0].executeorder = 1;
     configuration.getwid[0].tryorder = 1;
-    configuration.getwid[0].dothis = 'offlinegetwid';
+    configuration.getwid[0].dothis = 'getFromAngular';
+//    configuration.getwid[0].dothis = 'offlinegetwid';
     configuration.getwid[0].server = 'getwid';
     configuration.getwid[0].dofn = offlinegetwid;
     configuration.getwid[0].params = {};
